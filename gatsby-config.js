c2eb364256e42167b96aaee5e4aed69204659657
groninge01 <groninge@hotmail.com>
2019-08-16 22:08:18 +0200

    Update global settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Read on...',
+    siteUrl: 'https://www.readon.ml'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'Read on',
+        short_name: 'Read on',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
